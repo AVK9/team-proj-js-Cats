b7f9dbe163080f8da7f41662a6aa0bd09e77277c
AlexKat <katsubo79@gmail.com>
2023-11-09 09:52:39 +0200

    2.

--- src/js/part-all-categories.js
+++ src/js/part-all-categories.js
@@ -2,21 +2,21 @@ import Notiflix from 'notiflix';
 import listCategory from './axios'
 
 const catList = document.querySelector('.categoriesList');
-
-try {
-const resp = listCategory();
-
-markupCatList(resp)
-  }
-  catch (error) {
-    console.log(error);
-    Notiflix.Notify.failure('Error');
-  }
+listCategory()
+    .then(resp => {
+    markupCatList(resp);
+  
+    })
+    .catch(error => {
+        Notiflix.Notify.failure('Error');
+        console.log(error)
+    });
 
 function markupCatList(respArr) {
    const categoriesList = respArr.map(({list_name}
    ) => `
        <li class="categoryItem">${list_name}</li>
        `).join("");
-    catList.innerHTML = categoriesList;
+    const itemFirstEl = '<li class="categoryItem">All categories</li>';
+    catList.innerHTML = itemFirstEl + categoriesList;
 }
