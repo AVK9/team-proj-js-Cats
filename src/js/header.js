d4c218d5f677271cd454b65ca9f57556dea65fca
Exx1piee <130587927+Exx1piee@users.noreply.github.com>
2023-11-10 17:38:05 +0200

    first all-categories patch

--- src/js/header.js
+++ src/js/header.js
@@ -2,4 +2,5 @@ const themeSwitch = document.getElementById('themeSwitch');
 
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-mode', themeSwitch.checked);
+
 });
