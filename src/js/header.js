4aaf678dd8ef9612a2caf60da77e5b73bdab0dbc
Exx1piee <130587927+Exx1piee@users.noreply.github.com>
2023-11-10 21:46:42 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -2,7 +2,6 @@ const themeSwitch = document.getElementById('themeSwitch');
 const header = document.querySelector('.header');
 const shoppingListLink = document.querySelector('.a-shoppinglist');
 const mainContent = document.querySelector('.main-content-page');
-
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-mode', themeSwitch.checked);
   header.classList.toggle('header-dark-mode', themeSwitch.checked);
@@ -11,5 +10,4 @@ themeSwitch.addEventListener('change', () => {
     themeSwitch.checked
   );
   mainContent.classList.toggle('dark-mode-main-content', themeSwitch.checked);
-
 });
