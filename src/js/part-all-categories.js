dfbcd36a7eb79ca651153a4f738e0ed0b9a6cb9c
AlexKat <katsubo79@gmail.com>
2023-11-14 11:44:45 +0200

    +colab

--- src/js/part-all-categories.js
+++ src/js/part-all-categories.js
@@ -26,69 +26,69 @@ function markupCatList(respArr) {
 }
 ////////////////////////////////////////////////////////////
 
-// async function allBooksBackend() {
-//     const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
-//     return await fetch(`${BASE_URL}`)
-//        .then((resp) => {
-//     if (!resp.ok) {
-//       throw new Error(resp.statusText);
-//     }
-//     return resp.json();
-//   });
-// }
-// //---------------------------
+async function allBooksBackend() {
+    const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
+    return await fetch(`${BASE_URL}`)
+       .then((resp) => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+//---------------------------
 
-// await allBooksBackend()
-//     .then((data) => {
-//       console.log(data);
-//        createPageAllBooks(data);
-//   })
-//     .catch((err) => console.log(err));
+await allBooksBackend()
+    .then((data) => {
+      console.log(data);
+       createPageAllBooks(data);
+  })
+    .catch((err) => console.log(err));
         
 
     
-// function createPageAllBooks(respArr) {
-//     const headPage = `<h1 class="head-main-h">Best Sellers
-//   <span class="head-main-h head-main-hdecor">
-//   Books
-//   </span>
-//    </h1>`;
-//     let partBoxAllBooks = '';
-//     let pageAllBooks = '';
-// console.log(respArr);
-//     for (let i = 0; i < numberBlocksPage; i++) {
-//     let categoryName = respArr[i].list_name;
-//     let categoryArr = respArr[i].books;
-//     console.log(categoryArr);
+function createPageAllBooks(respArr) {
+    const headPage = `<h1 class="head-main-h">Best Sellers
+  <span class="head-main-h head-main-hdecor">
+  Books
+  </span>
+   </h1>`;
+    let partBoxAllBooks = '';
+    let pageAllBooks = '';
+console.log(respArr);
+    for (let i = 0; i < numberBlocksPage; i++) {
+    let categoryName = respArr[i].list_name;
+    let categoryArr = respArr[i].books;
+    console.log(categoryArr);
     
-//     const pageMarkupAllBooks = categoryArr.map(({ _id,
-//         author,
-//         book_image,
-//         title,}
-//    ) => `
-//       <li class="part-cards-list-item" id="${_id}" >
-//          <img class="images-prev" src="${book_image}" alt="${title}" width ='180'>
-//          <p class="info-item-title">${title}</p>
-//          <p class="info-item-author">${author}</p>
-//       </li>
-//        `).join("");
+    const pageMarkupAllBooks = categoryArr.map(({ _id,
+        author,
+        book_image,
+        title,}
+   ) => `
+      <li class="part-cards-list-item" id="${_id}" >
+         <img class="images-prev" src="${book_image}" alt="${title}" width ='180'>
+         <p class="info-item-title">${title}</p>
+         <p class="info-item-author">${author}</p>
+      </li>
+       `).join("");
     
-//       partBoxAllBooks =
-//       `<div class="best-sellers-group">
-//       <div class="bestellers-part">
-//               <p class="bestsellers-part-name">${categoryName}</p>
-//               <ul class="part-cards-list">
-//                   ${pageMarkupAllBooks}
-//               </ul>
-//           </div></div>
-//           <button class="btn-best-seemore" category = "${categoryName}">See More</button>
-//       `
-//         pageAllBooks += partBoxAllBooks;
-//   }
-//   const allPartToBox= `<div class="all-part-to-box">${pageAllBooks}</div>`
-//     // refs.catPage.insertAdjacentHTML('beforeend', headPage + pageAllBooks);
-//     mainContentPage.innerHTML = headPage + allPartToBox;
-// }
+      partBoxAllBooks =
+      `<div class="best-sellers-group">
+      <div class="bestellers-part">
+              <p class="bestsellers-part-name">${categoryName}</p>
+              <ul class="part-cards-list">
+                  ${pageMarkupAllBooks}
+              </ul>
+          </div></div>
+          <button class="btn-best-seemore" category = "${categoryName}">See More</button>
+      `
+        pageAllBooks += partBoxAllBooks;
+  }
+  const allPartToBox= `<div class="all-part-to-box">${pageAllBooks}</div>`
+    // refs.catPage.insertAdjacentHTML('beforeend', headPage + pageAllBooks);
+    mainContentPage.innerHTML = headPage + allPartToBox;
+}
 
 
 // //----------------
