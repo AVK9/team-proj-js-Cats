c399d7daf7fa0034f60a31d2936580d87f38c814
Voinash Denys <desdenisandqwerty@gmail.com>
2023-11-14 08:59:09 +0200

    ls

--- src/js/shopping-main.js
+++ src/js/shopping-main.js
@@ -30,6 +30,240 @@ const elements = [
     publisher: 'Simon & Schuster Audio',
     title: "I'M GLAD MY MOM DIED",
   },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/0593593804?tag=NYTBSREV-20',
+    author: 'Prince Harry',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780593593806.jpg',
+    book_uri: 'nyt://book/0db35be1-02f0-5fe8-8fdc-d4a669b5852f',
+    contributor: 'by Prince Harry',
+    description:
+      'The Duke of Sussex details his struggles with the royal family, loss of his mother, service in the British Army and marriage to Meghan Markle. Read by the author. 15 hours, 39 minutes unabridged.',
+    publisher: 'Random House Audio',
+    title: 'SPARE',
+  },
+  {
+    list_name: 'Audio Nonfiction',
+    amazon_product_url: 'https://www.amazon.com/dp/1982185821?tag=NYTBSREV-20',
+    author: 'Jennette McCurdy',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781982185824.jpg',
+    book_uri: 'nyt://book/dade20d6-b303-510c-9687-48eab9308755',
+    contributor: 'by Jennette McCurdy',
+    description:
+      'The actress and filmmaker describes her eating disorders and difficult relationship with her mother. Read by the author. 6 hours, 25 minutes unabridged.',
+    publisher: 'Simon & Schuster Audio',
+    title: "I'M GLAD MY MOM DIED",
+  },
 ];
 
 localStorage.setItem(LS_KEY, JSON.stringify(elements));
