04db7cfb391215709743d9ceef9a74e7cf300025
AlexKat <katsubo79@gmail.com>
2023-11-17 15:06:49 +0200

    cor=verstka

--- src/js/main-content-page.js
+++ src/js/main-content-page.js
@@ -109,9 +109,9 @@ function onSelectedCategory(respArr) {
               alt="${title}" width ='180' loading="lazy" />
               <p class="hover-text-card">quick view</p>
               </div>
-                <div class="info">
-                   <p class="info-item-title"><b>${title}</b></p>
-                   <p class="info-item-author"><b>${author}</b></p>
+                <div class="info-block-card">
+                   <p class="infos-item-title">${title}</p>
+                   <p class="infos-item-author">${author}</p>
              </div>
        </li>
        `
